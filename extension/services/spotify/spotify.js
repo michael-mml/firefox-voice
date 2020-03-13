3531664e09c43a41f8a19a748723bc5577ea5d9d
Yufei Cui <yufei.cui@mail.utoronto.ca>
2020-03-13 01:22:26 -0400

    Yfc/spotify drm (#1)
    * change location of error thrown to spotify
    
    Co-authored-by: Michael <mikel.liu@mail.utoronto.ca>

--- extension/services/spotify/spotify.js
+++ extension/services/spotify/spotify.js
@@ -5,8 +5,15 @@ import { shouldDisplayWarning } from "../../limiter.js";
 
 class Spotify extends serviceList.Service {
   async playQuery(query) {
-    await this.initTab("/services/spotify/player.js");
-    await this.callTab("search", { query, thenPlay: true });
+    try {
+      await this.initTab("/services/spotify/player.js");
+      await this.callTab("search", { query, thenPlay: true });
+    } catch (e) {
+      if (e.message === "You must enable DRM.") {
+        e.displayMessage = "You must enable DRM.";
+      }
+      throw e;
+    }
     if (this.tabCreated) {
       const isAudible = await this.pollTabAudible(this.tab.id, 3000);
       if (!isAudible) {
